a2279363aab4c48774b310925f02b209cd1f7332
Jay Cabasag <jaycabasag1999@gmail.com>
2023-07-08 12:15:44 +0800

    Added main memo

--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,9 +1,47 @@
-import { useState } from 'react'
 import { Product } from '../context/types'
+import { addItem, deleteItem, updateItem } from '../states/cart/utils'
+import { Item } from '../states/cart/types'
+import { v4 as uuidv4 } from 'uuid';
+import { useSnapshot } from 'valtio';
+import { cartState } from '../states/cart/states';
+import { memo } from 'react';
 
-export const ProductCard = ({ product }: { product: Product }) => {
-    const [count, setCount] = useState(0)
-    const showToggler = count > 0
+export const ProductCard = memo(({ product }: { product: Product }) => {
+    const cartStates = useSnapshot(cartState)
+    const cartItems = cartStates.items
+    const cartItem = cartItems.find((item) => {
+        const itemId = item.product.id
+        return itemId === product.id
+    })
+    const cartCount = cartItem ? cartItem.count : 0
+    const showToggler = cartCount > 0
+
+    const handleAddItem = (product: Product) => {
+        const item = { product, id: uuidv4().toString(), count: 1 } as Item
+        addItem(item)
+    }
+
+    const handleIncreaseItem = (product: Product) => {
+        if (!cartItem?.id) return
+        const item = { product, id: cartItem.id, count: cartCount + 1 } as Item
+        // console.log('INcreasw', item)
+        updateItem(item)
+    }
+
+    const handleDecreaseItem = (product: Product) => {
+        if (!cartItem?.id) return
+        const item = { product, id: cartItem.id, count: cartCount - 1 } as Item
+
+        if (cartCount <= 1) {
+            const cartItemId = cartItem ? cartItem.id : null
+            if (cartItemId) {
+                deleteItem(cartItemId)
+            }
+            return
+        }
+
+        updateItem(item)
+    }
 
     return (
         <div className='flex flex-col items-start'>
@@ -19,14 +57,15 @@ export const ProductCard = ({ product }: { product: Product }) => {
             <p>${product.price}</p>
             <div className='mt-[15px] w-full'>
                 {showToggler ? <>
-                    <button className='px-2 border' onClick={() => setCount(count === 0 ? 0 : count - 1)}>-</button>
-                    <span className='px-2'>{count}</span>
-                    <button className='px-2 border' onClick={() => setCount(count + 1)}>+</button>
+                    <button className='px-2 border' onClick={() => handleDecreaseItem(product)}>-</button>
+                    <span className='px-2'>{cartCount}</span>
+                    <button className='px-2 border' onClick={() => handleIncreaseItem(product)}>+</button>
                 </>
                     :
-                    <button onClick={() => setCount(count + 1)}>Add to cart</button>
+                    <button onClick={() => handleAddItem(product)}>Add to cart</button>
                 }
             </div>
         </div>
     )
 }
+)

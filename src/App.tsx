bc2fc0ee9db04661aed13379556812fc15ba730d
Jay Cabasag <jaycabasag1999@gmail.com>
2023-07-08 23:39:19 +0800

    Added fixes

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,7 @@ import { useProducts } from './context/ProductsContext'
 import { Cart } from './components/Cart'
 import { useSnapshot } from 'valtio'
 import { cartState } from './states/cart/states'
-import { v4 as uuidv4 } from 'uuid'
-import { addItem, deleteItem, updateItem } from './states/cart/utils'
 import { Item } from './states/cart/types'
-import { useCallback, useState } from 'react'
 
 function App() {
   const cartStates = useSnapshot(cartState)
@@ -18,36 +15,6 @@ function App() {
   const isLoading = status === 'loading'
   const cartProducts = cartItems.map((item) => item.product)
   const totalProducts = cartProducts.length
-  const [selectedIdToUpdate, setSelectedIdToUpdate] = useState<string>('')
-
-  const handleAddItem = useCallback((product: Product) => {
-    const item = { product, id: uuidv4().toString(), count: 1 } as Item
-    addItem(item)
-  }, [])
-
-
-  const handleIncreaseItem = useCallback((product: Product, count: number, cartItemId: string) => {
-    if (!cartItemId) return
-    setSelectedIdToUpdate(cartItemId)
-    const item = { product, id: cartItemId, count: count + 1 } as Item
-    console.log(item)
-    updateItem(item)
-  }, [selectedIdToUpdate])
-
-
-  const handleDecreaseItem = useCallback((product: Product, count: number, cartItemId: string) => {
-    if (!cartItemId) return
-    setSelectedIdToUpdate(cartItemId)
-    const item = { product, id: cartItemId, count: count - 1 } as Item
-    console.log(item)
-    if (count <= 1) {
-      deleteItem(cartItemId)
-      return
-    }
-
-    updateItem(item)
-  }, [selectedIdToUpdate])
-
 
   return (
     <main className='relative'>
@@ -66,14 +33,14 @@ function App() {
             return itemId === product.id
           }) as Item
 
+          const count = cartItem ? cartItem.count : 0
+
           return (
             <ProductCard
               key={product.id}
+              count={count}
               product={product}
               item={cartItem}
-              handleAddItem={handleAddItem}
-              handleIncreaseItem={handleIncreaseItem}
-              handleDecreaseItem={handleDecreaseItem}
             />
           )
 
--- src/components/ProductCard.tsx
+++ src/components/ProductCard.tsx
@@ -1,20 +1,46 @@
 import { Product } from '../context/types'
 import { Item } from '../states/cart/types'
 import { memo } from 'react';
+import { v4 as uuidv4 } from 'uuid'
+import { addItem, deleteItem, updateItem } from '../states/cart/utils';
 
 interface ProductCardProps {
+    count: number,
     product: Product,
     item?: Item,
-    handleAddItem: (product: Product) => void
-    handleIncreaseItem: (product: Product, count: number, cartItemId: string) => void
-    handleDecreaseItem: (product: Product, count: number, cartItemId: string) => void
 }
 
-export const ProductCard = memo(({ product, item, handleAddItem, handleIncreaseItem, handleDecreaseItem }: ProductCardProps) => {
+export const ProductCard = memo(({ count, product, item }: ProductCardProps) => {
 
     const showToggler = Boolean(item?.count)
-    const count = item ? item.count : 0
     const cartItemId = item ? item.id : ''
+
+    const handleAddItem = (product: Product) => {
+        const item = { product, id: uuidv4().toString(), count: 1 } as Item
+        addItem(item)
+    }
+
+
+    const handleIncreaseItem = () => {
+        if (!cartItemId) return
+        const item = { product, id: cartItemId, count: count + 1 } as Item
+        updateItem(item)
+    }
+
+
+    const handleDecreaseItem = () => {
+        if (!cartItemId) return
+
+        const item = { product, id: cartItemId, count: count - 1 } as Item
+
+        if (count <= 1) {
+            deleteItem(cartItemId)
+            return
+        }
+
+        updateItem(item)
+    }
+
 
     return (
         <div className='flex flex-col items-start'>
@@ -30,9 +56,9 @@ export const ProductCard = memo(({ product, item, handleAddItem, handleIncreaseI
             <p>${product.price}</p>
             <div className='mt-[15px] w-full'>
                 {showToggler ? <>
-                    <button className='px-2 border' onClick={() => handleDecreaseItem(product, count, cartItemId)}>-</button>
+                    <button className='px-2 border' onClick={() => handleDecreaseItem()}>-</button>
                     <span className='px-2'>{count}</span>
-                    <button className='px-2 border' onClick={() => handleIncreaseItem(product, count, cartItemId)}>+</button>
+                    <button className='px-2 border' onClick={() => handleIncreaseItem()}>+</button>
                 </>
                     :
                     <button onClick={() => handleAddItem(product)}>Add to cart</button>
